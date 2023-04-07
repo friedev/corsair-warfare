81500b93b5e97777bc34a96b8de890bae4e6e104
Aaron Friesen <aaron@frie.dev>
2023-04-07 16:56:09 -0500

    Refactor wind implementation
    - Save wind area as scene
    - Disable wind until players are ready
    - Export wind area's ship references
    - Store wind as vector
    - Use circular region for wind area and resize to fit current arena

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,7 @@ extends Node
 
 @onready var ship1: Ship = %Ship1
 @onready var ship2: Ship = %Ship2
+@onready var wind_area: WindArea = %WindArea
 
 
 func set_ship_enabled(ship: Ship, enabled: bool) -> void:
@@ -14,8 +15,11 @@ func set_ship_enabled(ship: Ship, enabled: bool) -> void:
 func _ready() -> void:
 	self.set_ship_enabled(self.ship1, false)
 	self.set_ship_enabled(self.ship2, false)
+	self.wind_area.collision_shape.disabled = true
 
 
 func _on_customization_menu_players_ready() -> void:
 	self.set_ship_enabled(self.ship1, true)
 	self.set_ship_enabled(self.ship2, true)
+	self.wind_area.collision_shape.disabled = false
+	self.wind_area.wind_change_timer.start()
