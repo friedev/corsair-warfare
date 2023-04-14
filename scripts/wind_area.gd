d5d3afaacd7f29302997933acc00da06d137ea56
Aaron Friesen <aaron@frie.dev>
2023-04-14 16:47:45 -0500

    Add smooth wind changing over time

--- scripts/wind_area.gd
+++ scripts/wind_area.gd
@@ -5,26 +5,35 @@ signal wind_changed(wind: Vector2)
 
 @export var ship1: Ship
 @export var ship2: Ship
+@export var wind_change_rate: float
+@export var wind_noise_multiplier: float
 
-var wind_directions: Array[Vector2] = [
-	Vector2(1, 0),
-	Vector2(0, 0.5),
-	Vector2(0.5, 0.5),
-	Vector2(0, -1),
-	Vector2(0.25, 0),
-]
+var wind: Vector2:
+	set(value):
+		wind = value
+		self.set_gravity_direction(self.wind)
+		self.wind_changed.emit(self.wind)
 
-var wind: Vector2
+var wind_noise_x := FastNoiseLite.new()
+var wind_noise_y := FastNoiseLite.new()
+var wind_noise_position := 0.0
 
 @onready var collision_shape: CollisionShape2D = %CollisionShape2D
-@onready var wind_change_timer: Timer = %WindChangeTimer
 
 
-func _ready():
-	self.change_wind_direction()
+func _ready() -> void:
+	var seed := int(Time.get_unix_time_from_system())
+	self.wind_noise_x.seed = seed
+	self.wind_noise_y.seed = seed + 1
 
 
 func _physics_process(delta: float) -> void:
+	self.wind_noise_position += delta * self.wind_change_rate
+	self.wind = Vector2(
+		self.wind_noise_x.get_noise_1d(self.wind_noise_position) * self.wind_noise_multiplier,
+		self.wind_noise_y.get_noise_1d(self.wind_noise_position) * self.wind_noise_multiplier
+	).limit_length()
+
 	if self.wind.x != 0:
 		if ship1.linear_velocity.y > 16:
 			ship1.correctNYMovement(self.wind.x)
@@ -34,16 +43,3 @@ func _physics_process(delta: float) -> void:
 	if self.wind.y != 0:
 		if ship1.linear_velocity.x < -16 || ship1.linear_velocity.x > 16:
 			ship1.correctNXMovement(self.wind.y)
-
-
-func change_wind_direction():
-	var index = randi() % wind_directions.size()
-	self.wind =  wind_directions[index]
-#	var current_wind = Vector2(2, 0)
-	self.set_gravity_direction(self.wind)
-	self.wind_changed.emit(self.wind)
-	print("gravity direction:", self.wind)
-
-
-func _on_timer_timeout():
-	self.change_wind_direction()
