f60f2dbdb60968cfbac1c0a26c6b3556481c33f0
Aaron Friesen <aaron@frie.dev>
2023-04-17 15:55:28 -0500

    Add options menu
    - Add audio buses and assign audio players to them

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,6 +2,7 @@ extends MarginContainer
 
 
 signal game_started
+signal options
 
 
 func _ready() -> void:
@@ -15,3 +16,12 @@ func _on_play_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	self.get_tree().quit()
+
+
+func _on_options_button_pressed() -> void:
+	self.options.emit()
+	self.hide()
+
+
+func _on_options_menu_go_back() -> void:
+	self.show()
