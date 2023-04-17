53ada3f09f7c601e01ada43170ea5fb6ba1a9c5e
Aaron Friesen <aaron@frie.dev>
2023-04-17 12:35:08 -0500

    Start playing wind ambience on first wind change

--- scripts/wind_ambience.gd
+++ scripts/wind_ambience.gd
@@ -4,5 +4,6 @@ extends AudioStreamPlayer
 @export var max_volume_db: float
 
 func _on_wind_area_wind_changed(wind: Vector2) -> void:
+	if not self.playing:
+		self.play()
 	self.volume_db = min_volume_db + (max_volume_db - min_volume_db) * wind.length()
-	print(self.volume_db)
