8d245c9d68cba4ad24bbc67404f9a9959ee55260
Aaron Friesen <aaron@frie.dev>
2023-05-01 00:07:25 -0500

    Pause World instead of toggling specific nodes

--- scripts/wind_ambience.gd
+++ scripts/wind_ambience.gd
@@ -5,8 +5,6 @@ extends AudioStreamPlayer
 @export var max_volume_db: float
 
 func _process(delta: float) -> void:
-	if self.wind.enabled != self.playing:
-		self.playing = self.wind.enabled
 	self.volume_db = (
 		min_volume_db
 		+ (max_volume_db - min_volume_db) * self.wind.wind.length()
