fa151a13f5e6099eec0f27276412581e0854bb23
Aaron Friesen <aaron@frie.dev>
2023-04-30 23:17:25 -0500

    Link wind ambience to new Wind node

--- scripts/wind_ambience.gd
+++ scripts/wind_ambience.gd
@@ -1,9 +1,13 @@
 extends AudioStreamPlayer
 
+@export var wind: Wind
 @export var min_volume_db: float
 @export var max_volume_db: float
 
-func _on_wind_area_wind_changed(wind: Vector2) -> void:
-	if not self.playing:
-		self.play()
-	self.volume_db = min_volume_db + (max_volume_db - min_volume_db) * wind.length()
+func _process(delta: float) -> void:
+	if self.wind.enabled != self.playing:
+		self.playing = self.wind.enabled
+	self.volume_db = (
+		min_volume_db
+		+ (max_volume_db - min_volume_db) * self.wind.wind.length()
+	)
