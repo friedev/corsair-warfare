cdeb0c9b919d483ad8aeaf080c03280c27a5c101
Aaron Friesen <aaron@frie.dev>
2023-05-01 01:02:20 -0500

    Use linear_to_db() to set volume levels in Options

--- scripts/options_menu.gd
+++ scripts/options_menu.gd
@@ -11,21 +11,19 @@ func _on_back_button_pressed() -> void:
 
 func set_volume(bus_name: String, value: float) -> void:
 	var bus_index := AudioServer.get_bus_index(bus_name)
-	var volume_db := (value - 5) * 3
-	AudioServer.set_bus_volume_db(bus_index, volume_db)
-	AudioServer.set_bus_mute(bus_index, value == 0)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
 
 
 func _on_sound_slider_value_changed(value: float) -> void:
-	self.set_volume("Sound", value)
+	self.set_volume(&"Sound", value)
 
 
 func _on_music_slider_value_changed(value: float) -> void:
-	self.set_volume("Music", value)
+	self.set_volume(&"Music", value)
 
 
 func _on_ambience_slider_value_changed(value: float) -> void:
-	self.set_volume("Ambience", value)
+	self.set_volume(&"Ambience", value)
 
 
 func _on_menu_open() -> void:
