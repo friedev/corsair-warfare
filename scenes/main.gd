9ac930a2ae5039fdf2fa6dcc3f14f6b3554f7788
Aaron Friesen <aaron@frie.dev>
2023-05-05 16:00:17 -0500

    Add prototype of ship spawning

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,18 @@
 extends Node
 
+signal game_over
+
+const SHIP_SCENE := preload("res://scenes/world/ship/ship.tscn")
+
+const SHIP_TEXTURES := [
+	preload("res://scenes/world/ship/white_ship.png"),
+	preload("res://scenes/world/ship/black_ship.png"),
+]
+
 @onready var world: Node2D = %World
+@onready var camera: ShakeCamera2D = %Camera2D
+@onready var wind: Wind = %Wind
+@onready var music: Music = %Music
 @onready var hud_layer: CanvasLayer = %HUDLayer
 
 var game_active: bool:
@@ -10,14 +22,40 @@ var game_active: bool:
 		self.hud_layer.visible = self.game_active
 		self.get_tree().paused = not self.game_active
 
+var ships_alive: int
+
+func spawn_ship(details: PlayerDetails) -> void:
+	var ship: Ship = self.SHIP_SCENE.instantiate()
+	ship.cannon_fired.connect(self.music._on_ship_cannon_fired)
+	ship.damage_taken.connect(self.camera._on_ship_damage_taken)
+	ship.destroyed.connect(self._on_ship_destroyed)
+	self.world.add_child(ship)
+	ship.player = details.player
+	ship.nickname = details.nickname
+	# TODO let player choose texture, or choose based on index
+	ship.texture = self.SHIP_TEXTURES[0]
+	# TODO spawn at random safe location
+	ship.position.x += randf() * 64
+	ship.position.y += randf() * 64
+	ship.wind = self.wind
+
 
 func _ready() -> void:
 	self.game_active = false
 
 
-func _on_customization_menu_players_ready() -> void:
+func _on_game_restarted() -> void:
+	self.get_tree().reload_current_scene()
+
+
+func _on_lobby_menu_players_ready() -> void:
+	self.ships_alive = len(Globals.players)
+	for details in Globals.players.values():
+		self.spawn_ship(details)
 	self.game_active = true
 
 
-func _on_game_restarted() -> void:
-	self.get_tree().reload_current_scene()
+func _on_ship_destroyed(ship: Ship) -> void:
+	self.ships_alive -= 1
+	if self.ships_alive <= 1:
+		self.game_over.emit()
--- scenes/ui/menus/game_over_menu.gd
+++ scenes/ui/menus/game_over_menu.gd
@@ -5,11 +5,14 @@ signal game_restarted
 @onready var label: Label = %Label
 
 
-func _on_ship_destroyed(ship: Ship) -> void:
-	if not self.visible:
-		self.show()
-		self.label.text = "Ship %d Wins!" % (3 - ship.player)
-
-
 func _on_menu_button_pressed() -> void:
 	self.game_restarted.emit()
+
+
+func _on_main_game_over() -> void:
+	self.label.text = "Game Over"
+	for ship in self.get_tree().get_nodes_in_group(&"ships"):
+		if ship.health > 0.0:
+			self.label.text = "%s Wins" % ship.nickname
+			break
+	self.show()
