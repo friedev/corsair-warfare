212841560fcdc42dc566584cdc4dbe518d408b42
Aaron Friesen <aaron@frie.dev>
2024-05-04 17:13:53 -0500

    Resize wind particle emission box to fit viewport

--- scenes/wind/wind_particles.gd
+++ scenes/wind/wind_particles.gd
@@ -7,13 +7,19 @@ var velocity_min_base: float
 var velocity_max_base: float
 
 
+func update_emission_box() -> void:
+	var viewport_size := self.get_viewport_rect().size
+	var particle_material := self.process_material as ParticleProcessMaterial
+	particle_material.emission_box_extents = Vector3(viewport_size.x, viewport_size.y, 0)
+
+
 func _ready() -> void:
-	# TODO update when screen resizes
 	var particle_material := self.process_material as ParticleProcessMaterial
 	self.velocity_min_base = particle_material.initial_velocity_min
 	self.velocity_max_base = particle_material.initial_velocity_max
-	var viewport_size := self.get_viewport_rect().size
-	particle_material.emission_box_extents = Vector3(viewport_size.x, viewport_size.y, 0)
+	self.update_emission_box()
+
+	self.get_tree().get_root().size_changed.connect(_on_window_size_changed)
 
 
 func _process(delta: float) -> void:
@@ -24,3 +30,8 @@ func _process(delta: float) -> void:
 	var added_velocity := self.added_velocity_multiplier * self.wind.wind.length()
 	particle_material.initial_velocity_min = self.velocity_min_base + added_velocity
 	particle_material.initial_velocity_max = self.velocity_max_base + added_velocity
+
+
+func _on_window_size_changed() -> void:
+	self.update_emission_box()
+
